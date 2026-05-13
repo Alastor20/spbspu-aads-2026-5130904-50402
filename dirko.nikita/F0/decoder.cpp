741cbe2450d133795a6d39498063c3f61aa9e474
Alastor20 <nik_d.007@mail.ru>
2026-05-13 09:58:43 +0300

    rework decoder

--- dirko.nikita/F0/decoder.cpp
+++ dirko.nikita/F0/decoder.cpp
@@ -1,62 +1,76 @@
 #include "decoder.hpp"
-#include <cstdint>
 #include <cstring>
 #include <fstream>
 #include <stdexcept>
-#include "fstream"
 
-using namespace dirko;
-
-Audio::Audio(std::string str)
+void dirko::WavDecoder::Load(const std::string &filename, WavFile &outWav)
 {
-  if (str.substr(str.size() - 4) != ".wav")
-    throw std::invalid_argument("Can only read WAV files!");
-  load_wav(str);
-}
+  std::ifstream file(filename, std::ios::binary);
 
-void Audio::load_wav(std::string str)
-{
-  std::ifstream file(str, std::ios::binary);
   if (!file.is_open()) {
-    throw std::runtime_error("Cant read file");
+    throw std::runtime_error("Failed to open file");
   }
-  // Chunk
-  file.read(type, 4);
-  if (strcmp(type, "RIFF"))
-    throw std::runtime_error("Not a RIFF file!");
 
-  file.read(reinterpret_cast< char * >(&ChunkSize), sizeof(ChunkSize));
-  file.read(format, 4);
-  if (strcmp(format, "WAVE"))
-    throw std::runtime_error("Not a WAVE format!");
+  char riff[4];
+  file.read(riff, 4);
 
-  // 1st Subchunk
-  file.read(Subchunk1ID, 4);
-  if (strcmp(Subchunk1ID, "fmt "))
-    throw std::runtime_error("Missing fmt header!");
-
-  file.read(reinterpret_cast< char * >(&Subchunk1Size), sizeof(Subchunk1Size));
-  file.read(reinterpret_cast< char * >(&AudioFormat), sizeof(AudioFormat));
-  file.read(reinterpret_cast< char * >(&NumChannels), sizeof(NumChannels));
-  file.read(reinterpret_cast< char * >(&SampleRate), sizeof(SampleRate));
-  file.read(reinterpret_cast< char * >(&ByteRate), sizeof(ByteRate));
-  file.read(reinterpret_cast< char * >(&BlockAlign), sizeof(BlockAlign));
-  file.read(reinterpret_cast< char * >(&BitsPerSample), sizeof(BitsPerSample));
-
-  // 2nd Subchunk
-  file.read(Subchunk2ID, 4);
-  if (strcmp(Subchunk2ID, "data") != 0)
-    throw std::runtime_error("Missing data header!");
-  file.read(reinterpret_cast< char * >(&Subchunk2Size), sizeof(Subchunk2Size));
-  // Data
-
-  // Subchunk2Size = NumSamples * NumChannels * BitsPerSample/8
-  int NumSamples = Subchunk2Size / (NumChannels * (BitsPerSample / 8));
-
-  data = std::vector< std::pair< int16_t, int16_t > >(NumSamples);
-  for (int i = 0; i < NumSamples; i++) {
-    file.read(reinterpret_cast< char * >(&data[i].first), sizeof(int16_t));
-    file.read(reinterpret_cast< char * >(&data[i].second), sizeof(int16_t));
+  if (std::strncmp(riff, "RIFF", 4) != 0) {
+    throw std::runtime_error("Not a RIFF file");
   }
-  file.close();
+
+  uint32_t chunkSize;
+  file.read(reinterpret_cast< char * >(&chunkSize), 4);
+
+  char wave[4];
+  file.read(wave, 4);
+
+  if (std::strncmp(wave, "WAVE", 4) != 0) {
+    throw std::runtime_error("Not a WAVE file");
+  }
+
+  bool fmtFound = false;
+  bool dataFound = false;
+  for (size_t i = 0; !file.eof(); ++i) {
+    char chunkId[4];
+    uint32_t chunkSize = 0;
+
+    file.read(chunkId, 4);
+
+    if (file.eof())
+      break;
+
+    file.read(reinterpret_cast< char * >(&chunkSize), 4);
+
+    if (std::strncmp(chunkId, "fmt ", 4) == 0) {
+      fmtFound = true;
+
+      file.read(reinterpret_cast< char * >(&outWav.audioFormat), 2);
+      file.read(reinterpret_cast< char * >(&outWav.numChannels), 2);
+      file.read(reinterpret_cast< char * >(&outWav.sampleRate), 4);
+      file.read(reinterpret_cast< char * >(&outWav.byteRate), 4);
+      file.read(reinterpret_cast< char * >(&outWav.blockAlign), 2);
+      file.read(reinterpret_cast< char * >(&outWav.bitsPerSample), 2);
+
+      // Skip extra fmt bytes if present
+      if (chunkSize > 16) {
+        file.seekg(chunkSize - 16, std::ios::cur);
+      }
+    } else if (std::strncmp(chunkId, "data", 4) == 0) {
+      dataFound = true;
+
+      outWav.rawData.reserve(chunkSize);
+      file.read(reinterpret_cast< char * >(outWav.rawData[i]), chunkSize);
+    } else {
+      // Skip unknown chunk
+      file.seekg(chunkSize, std::ios::cur);
+    }
+  }
+
+  if (!fmtFound || !dataFound) {
+    throw std::runtime_error("Invalid WAV file");
+  }
+
+  if (outWav.audioFormat != 1) {
+    throw std::runtime_error("Only PCM supported");
+  }
 }
--- dirko.nikita/F0/decoder.hpp
+++ dirko.nikita/F0/decoder.hpp
@@ -2,77 +2,28 @@
 #define DECODER_HPP
 
 #include <cstdint>
-#include <stdexcept>
 #include <string>
-#include <utility>
-#include <vector>
+#include "../common/Vector.hpp"
+
 namespace dirko
 {
-  class Audio
+
+  struct WavFile
+  {
+    uint16_t audioFormat;
+    uint16_t numChannels;
+    uint32_t sampleRate;
+    uint32_t byteRate;
+    uint16_t blockAlign;
+    uint16_t bitsPerSample;
+
+    Vector< uint8_t > rawData;
+  };
+
+  class WavDecoder
   {
   public:
-    // Constructors
-    Audio(std::string str);
-    Audio() {};
-
-    // Read / Write files
-    void load_wav(std::string str);
-
-    // Get functions
-    // Subchunk2Size = NumSamples * NumChannels * BitsPerSample / 8 <==> NumSamples = Subchunk2Size /
-    // (NumChannels*(BitsPerSample / 8))
-    unsigned get_size()
-    {
-      return data.size();
-    }
-    unsigned get_sample_rate()
-    {
-      return SampleRate;
-    }
-    unsigned get_n_channels()
-    {
-      return NumChannels;
-    }
-
-    // Set Functions
-    void set_sample_rate(int n)
-    {
-      SampleRate = n;
-    }
-    void set_n_channels(int n)
-    {
-      if (n != 1 && n != 2)
-        throw std::invalid_argument("n can only be 1 (MONO) or 2 (STEREO)!");
-      NumChannels = n;
-    }
-
-    // Overloaded Operators
-    std::pair< short, short > &operator[](unsigned i)
-    {
-      return data[i];
-    }
-
-  protected:
-    char type[5];
-    char format[5];
-    char Subchunk1ID[5];
-    char Subchunk2ID[5];
-
-    int32_t ChunkSize;
-    int32_t Subchunk1Size;
-    int32_t SampleRate;
-    int32_t ByteRate;
-    int32_t Subchunk2Size;
-
-    int16_t AudioFormat;
-    int16_t NumChannels;
-    int16_t BlockAlign;
-    int16_t BitsPerSample;
-
-    // utility
-    uint32_t NumSamples;
-
-    std::vector< std::pair< int16_t, int16_t > > data;
+    void Load(const std::string &filename, WavFile &outWav);
   };
 }
 #endif
