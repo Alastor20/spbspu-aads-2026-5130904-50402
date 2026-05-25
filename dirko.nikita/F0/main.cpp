683b0db483d165cdc257752f0320c29e2e3af1e0
Alastor20 <nik_d.007@mail.ru>
2026-05-25 18:50:15 +0300

    rewrite main

--- dirko.nikita/F0/main.cpp
+++ dirko.nikita/F0/main.cpp
@@ -1,71 +1,38 @@
 #include <iostream>
-#include <SDL2/SDL.h>
-#include "decoder.hpp"
+#include <limits>
+#include <string>
+#include "cmds.hpp"
+#include "player.hpp"
+#include "robin.hpp"
 
-#define MUS_PATH "C1.wav"
-
-bool PlayWav(dirko::WavFile &wav, double dur)
-{
-  if (SDL_Init(SDL_INIT_AUDIO) != 0) {
-    std::cout << SDL_GetError() << "\n";
-    return false;
-  }
-
-  SDL_AudioSpec spec{};
-  spec.freq = wav.sampleRate;
-  spec.channels = wav.numChannels;
-
-  switch (wav.bitsPerSample) {
-  case 8:
-    spec.format = AUDIO_U8;
-    break;
-
-  case 16:
-    spec.format = AUDIO_S16LSB;
-    break;
-
-  case 32:
-    spec.format = AUDIO_S32LSB;
-    break;
-
-  default:
-    std::cout << "Unsupported bit depth\n";
-    return false;
-  }
-
-  spec.samples = 4096;
-  spec.callback = nullptr;
-
-  SDL_AudioDeviceID device = SDL_OpenAudioDevice(nullptr, 0, &spec, nullptr, 0);
-
-  if (!device) {
-    std::cout << SDL_GetError() << "\n";
-    return false;
-  }
-
-  SDL_QueueAudio(device, wav.rawData.getData(), wav.rawData.getSize());
-
-  SDL_PauseAudioDevice(device, 0);
-
-  double duration = static_cast< double >(wav.rawData.getSize()) / wav.byteRate;
-  if (dur > duration) {
-    return false;
-  }
-  SDL_Delay(static_cast< Uint32 >(dur * 1000));
-
-  SDL_CloseAudioDevice(device);
-  SDL_Quit();
-
-  return true;
-}
 int main()
 {
-  dirko::WavFile wav;
-  Load(MUS_PATH, wav);
+  dirko::playlists_t db(5, .7);
+  db.add("DEFAULT", dirko::playlist_t());
+  dirko::RobinTable< std::string, dirko::cmd_t > cmds(20, .7);
+  cmds.add("play", dirko::play);
+  cmds.add("add", dirko::add);
+  cmds.add("remove", dirko::remove);
+  cmds.add("rename", dirko::rename);
+  cmds.add("playlist_add", dirko::playlist_add);
+  cmds.add("playlist_remove", dirko::playlist_remove);
+  cmds.add("playlist_rename", dirko::playlist_rename);
+  cmds.add("playlist_add_track", dirko::playlist_add_track);
+  cmds.add("playlist_remove_track", dirko::playlist_remove_track);
+  cmds.add("playlist_list", dirko::playlist_list);
+  cmds.add("playlist_merge", dirko::playlist_merge);
 
-  double duration = static_cast< double >(wav.rawData.getSize()) / wav.byteRate;
-  std::cout << duration << '\n';
-  double dur = 0;
-  std::cin >> dur;
-  PlayWav(wav, dur);
+  std::string cmd;
+  while (!std::cin.eof()) {
+    std::cin >> cmd;
+    try {
+      if (std::cin.fail()) {
+        std::cin.clear(std::cin.rdstate() & ~std::ios::failbit);
+      }
+      cmds.get(cmd)(std::cin, std::cout, db);
+    } catch (const std::exception &e) {
+      std::cout << '<' << e.what() << ">\n";
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
 }
