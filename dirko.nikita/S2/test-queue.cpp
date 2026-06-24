e72569d40f559d96a3bf1edb191fc9516fb70879
Alastor20 <nik_d.007@mail.ru>
2026-06-24 21:18:34 +0300

    add tests

--- dirko.nikita/S2/test-queue.cpp
+++ dirko.nikita/S2/test-queue.cpp
@@ -42,4 +42,48 @@ BOOST_AUTO_TEST_CASE(test_move_push)
 
   BOOST_CHECK_EQUAL(queue.get(), "faxryzen");
 }
+BOOST_AUTO_TEST_CASE(test_emplace)
+{
+  Queue< std::pair< int, std::string > > queue;
+
+  queue.emplace(20, "queue");
+
+  BOOST_CHECK_EQUAL(queue.size(), 1);
+  BOOST_CHECK_EQUAL(queue.get().first, 20);
+  BOOST_CHECK_EQUAL(queue.get().second, "queue");
+}
+
+BOOST_AUTO_TEST_CASE(test_emplace_multiple)
+{
+  Queue< int > queue;
+
+  queue.emplace(1);
+  queue.emplace(2);
+  queue.emplace(3);
+
+  BOOST_CHECK_EQUAL(queue.size(), 3);
+  BOOST_CHECK_EQUAL(queue.get(), 1);
+}
+
+BOOST_AUTO_TEST_CASE(test_emplace_move)
+{
+  Queue< std::string > queue;
+
+  std::string str = "move_queue";
+
+  queue.emplace(std::move(str));
+
+  BOOST_CHECK_EQUAL(queue.size(), 1);
+  BOOST_CHECK_EQUAL(queue.get(), "move_queue");
+}
+
+BOOST_AUTO_TEST_CASE(test_emplace_empty)
+{
+  Queue< std::string > queue;
+
+  queue.emplace("hello");
+
+  BOOST_CHECK(!queue.empty());
+  BOOST_CHECK_EQUAL(queue.get(), "hello");
+}
 BOOST_AUTO_TEST_SUITE_END()
--- dirko.nikita/S2/test-stack.cpp
+++ dirko.nikita/S2/test-stack.cpp
@@ -41,5 +41,48 @@ BOOST_AUTO_TEST_CASE(test_move_push)
 
   BOOST_CHECK_EQUAL(queue.get(), "faxryzen");
 }
+BOOST_AUTO_TEST_CASE(test_emplace)
+{
+  Stack< std::pair< int, std::string > > stack;
 
+  stack.emplace(10, "stack");
+
+  BOOST_CHECK_EQUAL(stack.size(), 1);
+  BOOST_CHECK_EQUAL(stack.get().first, 10);
+  BOOST_CHECK_EQUAL(stack.get().second, "stack");
+}
+
+BOOST_AUTO_TEST_CASE(test_emplace_multiple)
+{
+  Stack< int > stack;
+
+  stack.emplace(1);
+  stack.emplace(2);
+  stack.emplace(3);
+
+  BOOST_CHECK_EQUAL(stack.size(), 3);
+  BOOST_CHECK_EQUAL(stack.get(), 3);
+}
+
+BOOST_AUTO_TEST_CASE(test_emplace_move)
+{
+  Stack< std::string > stack;
+
+  std::string str = "move_stack";
+
+  stack.emplace(std::move(str));
+
+  BOOST_CHECK_EQUAL(stack.size(), 1);
+  BOOST_CHECK_EQUAL(stack.get(), "move_stack");
+}
+
+BOOST_AUTO_TEST_CASE(test_emplace_empty)
+{
+  Stack< std::string > stack;
+
+  stack.emplace("hello");
+
+  BOOST_CHECK(!stack.empty());
+  BOOST_CHECK_EQUAL(stack.get(), "hello");
+}
 BOOST_AUTO_TEST_SUITE_END()
